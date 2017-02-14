346b811761cc1fd302d29a65913060a98b9ccca9
Chris Bertrand <cjb_bjc@hotmail.com>
2017-02-14 11:05:27 +0000

    Initial Pizza Commit

--- HashCode2017Pizza/Program.cs
+++ HashCode2017Pizza/Program.cs
@@ -11,14 +11,72 @@ namespace HashCode2017Pizza
         static void Main(string[] args)
         {
             var program = new Program();
-            program.cutPizz();
+            program.Initialise();
         }
 
-        void cutPizz()
+        public int Rows { get; set; }
+        public int Collumns { get; set; }
+        public int MinIngredientsPerSlice { get; set; }
+        public int MaxCellsPerSlice { get; set; }
+
+        public string[,] Pizza { get; set; }
+        public string Tomato = "T";
+        public string Mushroom = "M";
+
+        void Initialise()
+        {
+            var FileDetails = System.IO.File.ReadAllLines("C:\\small.in");
+
+            var PizzaConfig = FileDetails.First();
+            Rows = PizzaConfig[0];
+            Collumns = PizzaConfig[1];
+            MinIngredientsPerSlice = PizzaConfig[2];
+            MaxCellsPerSlice = PizzaConfig[3];
+
+            Pizza = new string[Rows, Collumns];
+
+            var PizzaDetails = FileDetails.Skip(1);
+            //Draw the Pizza
+            CreatePizza(PizzaDetails);
+            CutPizza();
+        }
+
+        void CutPizza()
         {
 
         }
 
+        void CreatePizza(IEnumerable<string> PizzaDetails)
+        {
+            //for (int row = 0; row < Rows; row++)
+            //  {
+            //      for (int col = 0; col < Collumns; col++)
+            //      {
+            //          Pizza[row, col] = PizzaDetails.Where(i => i.)
+            //      }
+            //  }
+            var pizzaRepresentation = new StringBuilder();
 
+            foreach (var line in PizzaDetails)
+            {
+                int lineNum = 0;
+                foreach (var cell in line)
+                {
+                   int cellNum = 0;
+
+                   //Create the pizza
+                   Pizza[lineNum, cellNum] = cell.ToString();
+                    //Draw the pizza
+                    pizzaRepresentation.Append(cell);
+                   cellNum++;
+                }
+                pizzaRepresentation.AppendLine();
+                lineNum++;
+            }
+            Console.Write(pizzaRepresentation);
+
+            // debugging point to view console.
+            var data = false;
+        }
     }
 }
